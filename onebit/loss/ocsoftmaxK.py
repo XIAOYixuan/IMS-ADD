bug fix: no need to use weight parameters cuz we average the loss across num_samples ref #2

onebit/loss/ocsoftmaxK.py
@@ -29,9 +29,6 @@ class OCSoftmaxK(BaseLoss):
         self.m_real = getattr(loss_config, 'm_real', 0.9)
         self.m_fake = getattr(loss_config, 'm_fake', 0.2)
         self.alpha = getattr(loss_config, 'alpha', 20.0)
-        raw_real_weight = loss_config.get('real_weight', 1.0)
-        self.real_weight = raw_real_weight / (1.0 + raw_real_weight)
-        self.fake_weight = 1.0 - self.real_weight
         self.softplus = nn.Softplus()
         # fake weight default 1
 
@@ -49,10 +46,10 @@ class OCSoftmaxK(BaseLoss):
 
         total_loss = torch.tensor(0.0, device=real_scores.device, dtype=real_scores.dtype)
         if real_scores.numel() > 0:
-            real_loss = self.real_weight * self.softplus(self.alpha * (self.m_real - real_scores)).mean(dim=0)
+            real_loss = self.softplus(self.alpha * (self.m_real - real_scores)).mean(dim=0)
             total_loss += real_loss.sum()
         if fake_scores.numel() > 0:
-            fake_loss = self.fake_weight * self.softplus(self.alpha * (fake_scores - self.m_fake)).mean(dim=0)
+            fake_loss = self.softplus(self.alpha * (fake_scores - self.m_fake)).mean(dim=0)
             total_loss += fake_loss.sum()
         
         return LossOutput(loss=total_loss)